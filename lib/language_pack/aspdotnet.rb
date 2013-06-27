a968f2c0f2668ab5a75ac36c6fe3effcde40f36c
Luke Bakken <luke@bowbak.org>
2013-06-26 19:53:12 -0700

    Tweaks to ASP.NET buildpack.

--- lib/language_pack/aspdotnet.rb
+++ lib/language_pack/aspdotnet.rb
@@ -14,13 +14,20 @@ module LanguagePack
     end
 
     def compile
-      resources_dir = File.expand_path('../../../resources/iishost', __FILE__)
-      FileUtils.cp_r(resources_dir, build_path)
+      resources_dir = File.join(File.expand_path('../../../resources/iishost', __FILE__), '.') # NB: end with slash-dot to copy contents of iishost, not dir itself
+      Dir.chdir(build_path) do
+        FileUtils.mkdir_p(iishost_dir)
+        FileUtils.cp_r(resources_dir, iishost_dir)
+      end
+    end
+
+    def iishost_dir
+      '.iishost'
     end
 
     def default_process_types
       {
-        "web" => "iishost.exe"
+        "web" => ".\\.iishost\\iishost.exe"
       }
     end
 
